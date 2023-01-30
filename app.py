7b1b74091ca1d1e8e0f323a8c17857dd49558e9d
gerardoanuglo <85320743+gerardoanuglo@users.noreply.github.com>
2023-01-30 11:41:31 -0800

    Fixed bugs in the app and scraping file. Also updated the visuals of the website.

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 #import dependencies
 from flask import Flask, render_template, redirect, url_for
-from scrape_mars import scrape_all
+import scrape_mars
 from flask_pymongo import PyMongo
 
 #create an app
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
 
 #create main route that renders html file
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
-
+    mars_ap = mongo.db.mars_co.find_one()
+    return render_template("index.html", mars_html=mars_ap)
 
 #create route that will import scrape_mars.py script and call scrape function
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
+    mars_ap = mongo.db.mars_co
     mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return redirect('/')
+    mars_ap.insert_one(mars_data)
+    return redirect('/', code=302)
 
 if __name__ == "__main__":
     app.run()
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,13 +1,12 @@
 #import dependencies
 from bs4 import BeautifulSoup
 import pandas as pd
-from splinter import Browser
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
-    # Set up Splinter
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    Browser = Browser('chrome', **executable_path, headless=True)
+    # Set up driver
+    Browser = webdriver.Chrome(ChromeDriverManager().install())
 
     news_title, news_p = mars_news(Browser)
 
@@ -18,52 +17,31 @@ def scrape_all():
         "featured_image": mars_images(Browser),
         "facts": mars_facts(),
         "hemispheres": mars_hemi(Browser),
-        "image_titles": hemi_titles(Browser)
     }
 
     # Stop webdriver and return data
     Browser.quit()
     return data
 
-
 def mars_news(Browser):
     #Visit mars url
     url = 'https://redplanetscience.com/'
-    Browser.visit(url)
+    Browser.get(url)
 
     #scrape page into soup object
-    html=Browser.html
+    html=Browser.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    #the tag that contains all news titles and paragraphs
-    sections = soup.find_all('div', class_="content_title")
-    containers = soup.find_all('div', class_="article_teaser_body")
+    # add try/except for error handling
+    try:
+        # Use the parent element to find the first a tag and save it as `news_title`
+        slide_elem = soup.select_one('div.list_text')
+        news_title = slide_elem.find('div', class_='content_title').get_text()
 
-    #list of titles and paragraphs
-    title_list=[]
-    p_list=[]
-
-    # Loop through returned sections for titles
-    for section in sections:
-
-        #retrieve the news title
-        title = section.text
-
-        #append list with each title
-        title_list.append(title)
-
-    # Loop through returned containers for paragraphs
-    for container in containers:
-
-        #retrieve the news paragraph
-        text = container.text
-
-        #append list with each paragraph
-        p_list.append(text)
-
-    #the most recent title with its corresponding teaser paragraph.
-    news_title = title_list[0]
-    news_p = p_list[0]
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
 
     #return
     return news_title, news_p
@@ -71,17 +49,22 @@ def mars_news(Browser):
 def mars_images(Browser):
     #visit mars space image website
     JPL_url = "https://spaceimages-mars.com/"
-    Browser.visit(JPL_url)
+    Browser.get(JPL_url)
 
     #scarpe page into soup object
-    JPL_html = Browser.html
+    JPL_html = Browser.page_source
     JPL_soup = BeautifulSoup(JPL_html, 'html.parser')
     
-    #find the image URL for the current Featured Mars Image
-    img_tag = JPL_soup.find("img", class_="headerimage fade-in")
+    # Add try/except for error handling
+    try:
+        #find the image URL for the current Featured Mars Image
+        img_tag = JPL_soup.find("img", class_="headerimage fade-in")
 
-    #store jpg file name in a varible
-    img_file_name = img_tag.get("src")
+        #store jpg file name in a varible
+        img_file_name = img_tag.get("src")
+
+    except AttributeError:
+        return None
 
     #create full url for img
     featured_image_url = (f"{JPL_url}{img_file_name}")
@@ -120,11 +103,13 @@ def mars_hemi(Browser):
 
     #create a loop that goes through each url extracting the images
     for url in list_urls:
+        hemisphere_dict = {}
+
         #visit JPL mars space image website
-        Browser.visit(url)
+        Browser.get(url)
 
         #create html variable
-        temp_html = Browser.html
+        temp_html = Browser.page_source
 
         #create soup object
         temp_soup = BeautifulSoup(temp_html, 'html.parser')
@@ -132,10 +117,10 @@ def mars_hemi(Browser):
         #look for tag that contains the image URL to the full-resolution image
         temp_a = temp_soup.find_all("a")
 
-        #create a temp list for href links, that also resets for each website
+        #create a temp list that resets for each website
         href_list=[]
 
-        #create a loop through each "a" tag for href image links
+        #create a loop through each a tag for href image links
         for a in temp_a:
             #pull each href link and append it to the href_list
             link=a.get("href")
@@ -143,53 +128,22 @@ def mars_hemi(Browser):
             href_list.append(link)
 
         #create full url with href link
-        #item number 5 in list is the desired link
-        img_link= (f"https://marshemispheres.com/schiaparelli.html/{href_list[3]}")
+        img_link= (f"https://marshemispheres.com/{href_list[3]}")
+
+        #add img url to hem dict
+        hemisphere_dict['img_url'] = img_link
+
+        #look for tag that contains the image URL to the full-resolution image
+        temp_b = temp_soup.find("h2", class_ = "title")
+
+        # Get Hemisphere title
+        hemisphere_dict['title'] = temp_b.text
 
         #append this image url to hemisphere_image list
-        hemi_imgs_list.append(img_link) 
-    
+        hemi_imgs_list.append(hemisphere_dict)  
+
     #return list of hemisphere images
     return hemi_imgs_list
-
-def hemi_titles(Browser):
-    #each individual url for a specific hemisphere of mars
-    cerberus_url="https://marshemispheres.com/cerberus.html"
-    schiaparelli_url="https://marshemispheres.com/schiaparelli.html"
-    syrtis_url="https://marshemispheres.com/syrtis.html"
-    valles_url="https://marshemispheres.com/valles.html"
-
-    #create a list of urls to loop through
-    list_urls= [cerberus_url, schiaparelli_url, syrtis_url, valles_url]
-    hemispheres=[]
-
-    #create a loop that goes through each url extracting the image title
-    for url in list_urls:
-        #visit JPL mars space image website
-        Browser.visit(url)
-
-        #create html variable
-        temp_html = Browser.html
-
-        #create soup object
-        temp_soup = BeautifulSoup(temp_html, 'html.parser')
-
-        #look for tag that contains the image title text to the sample image and the tag for the img url
-        temp_b = temp_soup.find("h2", class_ = "title")
-        temp_c = temp_soup.find("a", text="Sample").get("href")
-
-        #create a dictionary to add to the list
-        temp_dict = {
-        "title": temp_b,
-        "img_url": temp_c
-        }
-
-        #append to hemispheres list
-        hemispheres.append(temp_dict)
-
-    return hemispheres
-
-
 
 if __name__ == "__main__":
 
